543404284e2002c74831f50005752f0d87c50c33
miflck <contact@michaelflueckiger>
2019-07-16 10:29:48 +0200

    clean up

--- app.js
+++ app.js
@@ -1,59 +1,39 @@
 
 // Threejs
 // Get a reference to the container element that will hold our scene
-const container = document.querySelector( '#scene-container' );
+var container;
 // create a Scene
 const scene = new THREE.Scene();
 
 // create the renderer
 //const renderer = new THREE.WebGLRenderer({ antialias: true,alpha: true,preserveDrawingBuffer: true  });
-const renderer = new THREE.WebGLRenderer({ antialias: false,alpha: true,preserveDrawingBuffer: true  });
-
+const renderer = new THREE.WebGLRenderer({ antialias: false, alpha: true, preserveDrawingBuffer: true  });
 renderer.autoClearColor = false;
 
+var  stats;
+// create a clock for animations
 var clock = new THREE.Clock({autoStart:true});
 
+// Window and Pixel Vars
+let windowWidth=1920;
+let windowHeight=1080;
+let pixelScaleFact=1;
+let windowHalfY = windowHeight / 2
 
- // Create shortcuts for window size.
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
-
-
- // Create shortcuts for window size.
-//var windowWidth = window.innerWidth;
-//var windowHeight = window.innerHeight;
-
-//var windowWidth=1920;
-//var windowHeight=1200;
-
-
-var windowWidth=1920;
-var windowHeight=1080;
-var pixelScaleFact=1;
-
-
-//var windowWidth=1500;
-//var windowHeight=800;
-
-const windowHalfY = windowHeight / 2
 
 // Create a Camera
-const left = -windowWidth/2; 
-const right = windowWidth/2;
-const topB =	windowHeight/2;
-const bottom = -windowHeight/2;
-const near= 0;
-const far= 100;
-const camera = new THREE.OrthographicCamera( left, right, topB, bottom, near, far );
-
-
-
-
+let left = -windowWidth/2; 
+let right = windowWidth/2;
+let topB =	windowHeight/2;
+let bottom = -windowHeight/2;
+let near= 0;
+let far= 100;
+let camera = new THREE.OrthographicCamera( left, right, topB, bottom, near, far );
 
 // Winds
 var winds=[];
 let latestAngle=0;
-// angelRotation
+// rotation fact
 var randMin=5;
 var randMax=15;
 var rotationDurationMin=1000;
@@ -61,45 +41,8 @@ var rotationDurationMAx=2000;
 var windVelocityMin=15;
 var windVelocityMax=20;
 
-
-// VANES
-var numberOfVanes=[];
-var vanegeometry = new THREE.BufferGeometry();
-vanegeometry.dynamic = true;
-var diameter=18;
-
-
-var vanes = [];
-var testvanes = [];
-
-var line;
-const r = 100;
-const mouseY = 0;
-
-var  stats;
-
-
-var lineGeometry;
-var lineMaterial;
-
-//oldcolors
-//const colors=[0x005597, 0x000C78,0x0017E6,0x0012B0,0xA7C6ED,0x307FE2,0x13294B,0xC7DBF4,0xA7A8AA,0x000000] //(meine)
-
-const colors=[0xCAE4F6,0xAAD4EB,0x61AEE5,0x4E71B4,0x26539D]
-
-// music color
-//const colors=[0xF8485E,0xD0006F,0x702082,0xFF8200,0xFFCD00,0x5FE0B7,0x7A9A01,0x85B09A,0x88DBDF]
-
-
-
-// image
-
-//var imagedata;
-var isloaded=false;
-var imagesData=[];
-
-
-
+// wind timer
+var bIsTimed=true;
 var eraseWaveInitTime;
 var eraseWaveTimerDuration;
 var eraseWaveIntervalMin=1500;
@@ -116,10 +59,28 @@ var cycleWaveIntervalMin=6000;
 var cycleWaveIntervalMax=7000;
 
 
+// VANES
+var numberOfVanes;
+var vanes = [];
+var vanegeometry = new THREE.BufferGeometry();
+vanegeometry.dynamic = true;
+var diameter=18;
+var lineGeometry;
+var lineMaterial;
+var thickline;
+var plane;
 
-var bIsTimed=true;
+//oldcolors
+//const colors=[0x005597, 0x000C78,0x0017E6,0x0012B0,0xA7C6ED,0x307FE2,0x13294B,0xC7DBF4,0xA7A8AA,0x000000] //(meine)
+const colors=[0xCAE4F6,0xAAD4EB,0x61AEE5,0x4E71B4,0x26539D]
+// music color
+//const colors=[0xF8485E,0xD0006F,0x702082,0xFF8200,0xFFCD00,0x5FE0B7,0x7A9A01,0x85B09A,0x88DBDF]
 
 
+
+// Background images
+var isloaded=false;
+var imagesData=[];
 var imageIndex=0;
 
 
@@ -133,70 +94,51 @@ const CONTENT=1;
 var zeroStartX=windowWidth-50;
 var zeroStartY=0;
 
-			
-init();
-animate();
+var debugLog=true;
+
+
+// setup event listeners		
+window.addEventListener("mousedown", onMouseDown, false);
+window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener("touchstart", onTouchStart, false);
+document.addEventListener("keydown", onDocumentKeyDown, false);	
+
+
+/*
+	init();
+	animate();
+*/
+
 
 
 
 function init() {
-	var xpos=-windowWidth/2
-	var ypos=windowHeight/2
-	var opX=0
-	var opY=0
-	
+
+ 	container= document.querySelector( '#scene-container' );
+
+
+	// create all the vanes
 	createVanes(diameter);
 
-  	window.addEventListener("mousedown", onMouseDown, false);
-	window.addEventListener( 'resize', onWindowResize, false );
-	window.addEventListener("touchstart", onTouchDown, false);
-	document.addEventListener("keydown", onDocumentKeyDown, false);
-
-
-
-	camera.position.z = 1;
-
-	
+	// set up geometry
 	vanegeometry = createGeometry();
 	vanegeometry.computeBoundingSphere();
-
  	lineGeometry = new THREE.LineSegmentsGeometry().setPositions( vanegeometry.attributes.position.array);
  	lineGeometry.setColors( vanegeometry.attributes.color.array);
-
+	// set up material
  	lineMaterial = new THREE.LineMaterial( { vertexColors: THREE.VertexColors, linewidth: 4.5} );
 	lineMaterial.resolution.set( windowWidth,windowHeight); // important, for now...
-	var thickline = new THREE.LineSegments2( lineGeometry, lineMaterial );
+	 thickline = new THREE.LineSegments2( lineGeometry, lineMaterial );
 	scene.add( thickline );
-
 	// background plane
-	var plane = new THREE.Mesh( new THREE.PlaneGeometry( windowWidth, windowHeight ), new THREE.MeshBasicMaterial( { transparent: true, opacity: 0.25 } ) );
+	 plane = new THREE.Mesh( new THREE.PlaneGeometry( windowWidth, windowHeight ), new THREE.MeshBasicMaterial( { transparent: true, opacity: 0.25 } ) );
 	plane.position.z = -10;
 	scene.add( plane );
 
-	
-	
-	renderer.setPixelRatio( window.devicePixelRatio );
-	//renderer.setPixelRatio( 1);
 
 
-	renderer.setSize( windowWidth, windowHeight );
-	renderer.setClearColor( 0x000000, 0);
 
-	// add the automatically created <canvas> element to the page
-	container.appendChild( renderer.domElement );
-
-	stats = new Stats();
-	container.appendChild( stats.dom );
-
-	/*
-	if(typeof imagesData[0] === 'undefined') {
-		console.log("undef");
-	}
-	else {
-		 console.log("def");
-	}
-	*/
-
+	// start automatic timers
 	state=WAVE;
 	eraseWaveInitTime=getMilliseconds(clock);
 	eraseWaveTimerDuration=randomIntFromInterval(eraseWaveIntervalMin,eraseWaveIntervalMax);
@@ -205,7 +147,73 @@ function init() {
 	cycleWaveTimerDuration=randomIntFromInterval(cycleWaveIntervalMin,cycleWaveIntervalMax);
 
 
+	camera.position.z = 1;	
+	renderer.setPixelRatio( window.devicePixelRatio );
+	//renderer.setPixelRatio( 1);
+	renderer.setSize( windowWidth, windowHeight );
+	renderer.setClearColor( 0x000000, 1);
+	// add the automatically created <canvas> element to the page
+	container.appendChild( renderer.domElement );
+
+	stats = new Stats();
+	container.appendChild( stats.dom );
 }
+
+
+
+
+function animate() {
+	var millis=getMilliseconds(clock);
+		// update Wind	
+		winds.map((wind,i) =>{
+			wind.move(millis);
+	//		wind.display();
+			if(wind.getDeleteMe()){
+				removeEntity(wind);
+				winds.splice(i,1);
+			}
+  		})
+	
+	if(bIsTimed){
+		if(millis>eraseWaveInitTime+eraseWaveTimerDuration){
+   			makeRandomWind(true);
+    		eraseWaveInitTime=millis;
+   			eraseWaveTimerDuration=randomIntFromInterval(eraseWaveIntervalMin,eraseWaveIntervalMax);
+  		}
+
+	 	if(millis>cycleWaveInitTime+cycleWaveTimerDuration){
+	   		cycleImages();
+	   		makeRandomWind(true);
+	    	cycleWaveInitTime=millis;
+	    	cycleWaveTimerDuration=randomIntFromInterval(cycleWaveIntervalMin,cycleWaveIntervalMax);
+	    	eraseWaveInitTime=millis;
+	    	eraseWaveTimerDuration=3000//randomIntFromInterval(eraseWaveIntervalMin,eraseWaveIntervalMax);
+  		}
+	}
+  	
+  	switch(state){
+        case WAVE:
+        
+        break;
+
+        case CONTENT:
+
+        break;
+        }
+
+	updateGeometry();
+	requestAnimationFrame( animate );
+	render();
+	stats.update();
+}
+
+
+
+function render() {			
+	renderer.render( scene, camera );
+}
+
+
 
 
 function setMask(image){
@@ -246,9 +254,6 @@ function updateGeometry(){
 
 	for (let i = 0; i < numberOfVanes; i++) {
 
-
-
-
 		var vane=vanes[i];
   			winds.map((wind,i) =>{
             	let outer=check_a_point(vane.x,vane.y,wind.x,wind.y,wind.radius);
@@ -260,11 +265,8 @@ function updateGeometry(){
             })
   		
   		vane.update(millis);
-
-
 		var angle=vanes[i].getCurrentAngle;
 		var col=vanes[i].getStrokeColor;
-	
 		vector.x=vanel;
 		vector.y=0;
 
@@ -292,8 +294,6 @@ function updateGeometry(){
 		color[i*6+4]=col.g;
 		color[i*6+5]=col.b;
 
-
-	
 	}
 	//vanegeometry.attributes.position.needsUpdate = true;
 	//vanegeometry.attributes.color.needsUpdate = true;
@@ -313,8 +313,6 @@ function createGeometry() {
 	var ypos=windowHeight/2
 	var vanel=diameter;
 	var axis = new THREE.Vector3( 0, 0, 1 );
-
-
 
 	vanes.map(vane =>{
 		vertex.x = vane.position.x;
@@ -339,103 +337,6 @@ function createGeometry() {
 	return vanegeometry;
 }
 
-	function onWindowResize() {
-
-		//windowHalfY = window.innerHeight / 2;
-
-		//camera.aspect = window.innerWidth / window.innerHeight;
-		//camera.updateProjectionMatrix();
-
-		//renderer.setSize( window.innerWidth, window.innerHeight );
-
-	}
-
-	function onDocumentMouseMove( event ) {
-		//mouseY = event.clientY - windowHalfY;
-	}
-
-	function onDocumentMouseDown( event ) {
-	  	//winds.push(new Wind(event.clientX - windowHalfX,event.clientY - windowHalfY));
-	}
-
-
-	function onTouchDown(event) {
-
-   	makeRandomWind(1);
-       	if(Math.random()>0.7){
-       	    	cycleImages();
-       	    }
-
-	//var col=new THREE.Color( colors[Math.floor(Math.random()*colors.length)] );
-	//winds.push(new Wind(event.touches[0].clientX ,event.touches[0].clientY,0,col));
-		//makeRandomWind(0);
-
-}
-
-	function onDocumentTouchMove( event ) {
-	/*	if ( event.touches.length == 1 ) {
-			event.preventDefault();
-			mouseY = event.touches[ 0 ].pageY - windowHalfY;
-		}
-		*/
-	}
-
-
-function animate() {
-	var millis=getMilliseconds(clock);
-		// update Wind	
-		winds.map((wind,i) =>{
-			wind.move(millis);
-	//		wind.display();
-			if(wind.getDeleteMe()){
-				removeEntity(wind);
-				winds.splice(i,1);
-			}
-  		})
-	
-	if(bIsTimed){
-		if(millis>eraseWaveInitTime+eraseWaveTimerDuration){
-   			makeRandomWind(true);
-    		eraseWaveInitTime=millis;
-   			eraseWaveTimerDuration=randomIntFromInterval(eraseWaveIntervalMin,eraseWaveIntervalMax);
-  		}
-
-
-	 if(millis>cycleWaveInitTime+cycleWaveTimerDuration){
-   		cycleImages();
-   		makeRandomWind(true);
-    	cycleWaveInitTime=millis;
-    	cycleWaveTimerDuration=randomIntFromInterval(cycleWaveIntervalMin,cycleWaveIntervalMax);
-    	eraseWaveInitTime=millis;
-    	eraseWaveTimerDuration=3000//randomIntFromInterval(eraseWaveIntervalMin,eraseWaveIntervalMax);
-  }
-
-
-
-
-	}
-  	
-  	switch(state){
-        case WAVE:
-        
-        break;
-
-        case CONTENT:
-
-        break;
-        }
-
-	updateGeometry();
-	requestAnimationFrame( animate );
-	render();
-	stats.update();
-}
-
-
-
-	function render() {			
-		renderer.render( scene, camera );
-	}
 
 			
 
@@ -451,20 +352,16 @@ function setActive(vane,wind,millis){
 	var color = new THREE.Color();
 	color.setHSL( hsl[0], hsl[1], hsl[2]);*/
 
-
 	var col=wind.color;
 	var maskColor;
 
-
 	var color = getPixel( wind.imageData, vane.ox*pixelScaleFact,vane.oy*pixelScaleFact);
-		if(color.a==255){
-			vane.isOnMask=true;		
-		}else{
-			vane.isOnMask=false;
-		}
+	if(color.a==255){
+		vane.isOnMask=true;		
+	}else{
+		vane.isOnMask=false;
+	}
 
-
-	
   	switch(state){
         case WAVE:
           //updateVanes(mil);
@@ -499,7 +396,7 @@ function setActive(vane,wind,millis){
 		//vane.setColor(wind.color);
 		vane.setTargetAngle(angle,millis); 
 		vane.setDuration(duration);
-					vane.setColor(wind.color);
+		vane.setColor(wind.color);
 
 	}
 }
@@ -512,62 +409,49 @@ function setState(newState){
 
 	switch(state){
         case WAVE:
-        //setMask(actualImage);
-        //makeEraseWave(false);
-       // makeWave(false);
-
-   
-
         break;
 
         case CONTENT:
-        setMask(nullImage);
-       // makeWaveWithPosition(zeroStartX,zeroStartY);
-
-
-
+      //  setMask(nullImage);
         break;
       }
 
 }
 
 
-
 function makeRandomWind(isMasked){
+  	
+	// make startposition
   	var center= new THREE.Vector3( windowWidth/2,windowHeight/2,0);
   	var pos=new THREE.Vector3(-(windowWidth/3)*2,0,0);
 	var axis = new THREE.Vector3( 0, 0, 1);
 	//var angle = randomFloatFromInterval(0,2*Math.PI);
 	var angle = randomFloatFromInterval(-Math.PI/4,Math.PI/4);
-
-
 	pos.applyAxisAngle( axis, angle );
 	pos.add(center);
+
+	// get random color from array
 	var randNr=Math.floor(Math.random()*colors.length);
 	var col=new THREE.Color( colors[randNr]);
-	console.log("color "+randNr+' #' + col.getHex().toString(16));
+	if(debugLog)console.log("color "+randNr+' #' + col.getHex().toString(16));
 
 
     var vel = randomIntFromInterval(windVelocityMin,windVelocityMax);
-
-
+    // random rotation factor
  	var rand=randomIntFromInterval(randMin,randMax);
- 	//var rand=randomIntFromInterval(30,40);
-
-
 	var mult=1;
 	//if(Math.random()>0.5)mult=-1;
 	var angle=rand*(Math.PI/4)*mult;
     var dur=scale(rand,randMin,randMax,rotationDurationMin,rotationDurationMAx);
     //    var dur=scale(rand,30,80,3000,8000);
 
+    // delay in starting wave, not implemented yet
     var wait=0;
+    // get start time
 	var millis=getMilliseconds(clock);
-
-
 	if(isloaded){
 		var imagedata = getImageData(imagesData[imageIndex]);
-		console.log("data "+imagedata);
+		if(debugLog)console.log("data "+imagedata);
 		winds.push(new Wind(pos.x ,pos.y,vel,angle,dur,wait,millis,col,isMasked,imagedata));
 	}
 }
@@ -575,15 +459,7 @@ function makeRandomWind(isMasked){
 
 
 
-
-
-
-
-function removeEntity(object) {
-	var selectedObject = scene.getObjectById(object.name);
-	scene.remove( selectedObject );
-}
-
+// INPUT EVENTS
 
 function onMouseDown(event) {
 	makeRandomWind(1);
@@ -591,21 +467,14 @@ function onMouseDown(event) {
 	//winds.push(new Wind(event.clientX ,event.clientY,0,col));
 }
 
-
-
-function cycleImages(){
-	imageIndex=(imageIndex+1)%imagesData.length
-	//setMask(imagesData[imageIndex]);
-	console.log("cycle!"+imageIndex);
-}
-
-
 // movement
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     // w
     if (keyCode == 87) { 
-       	makeRandomWind(1);
+       	//makeRandomWind(1);
+
+       	setSize(randomIntFromInterval(1000,2500),randomIntFromInterval(500,1200),window.devicePixelRatio);
 
         //Q
     } else if (keyCode == 81) {
@@ -623,6 +492,29 @@ function onDocumentKeyDown(event) {
         makeRandomWind(false)
     }
 };
+
+
+
+
+
+function onWindowResize() {
+
+		//windowHalfY = window.innerHeight / 2;
+
+		//camera.aspect = window.innerWidth / window.innerHeight;
+		//camera.updateProjectionMatrix();
+
+		//renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onTouchStart(event) {
+   	makeRandomWind(1);
+    if(Math.random()>0.7){
+       	cycleImages();
+    }
+}
+
 
 
 
--- helper.js
+++ helper.js
@@ -1,3 +1,6 @@
+// HELPERS
+// random number interval
+
 function randomIntFromInterval(min, max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
@@ -5,7 +8,7 @@ function randomIntFromInterval(min, max){
 function randomFloatFromInterval(min, max){
       return (Math.random()*(max-min+1)+min);
 }
-
+// mapping
 const scale = (num, in_min, in_max, out_min, out_max) => {
      return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
@@ -38,26 +41,18 @@ function easeInOutQuad(t, b, c, d) {
     return -c/2 * (t*(t-2) - 1) + b;
 };
 
-
-
-
 function easeInOutQuint (t, b, c, d) {
 	if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
 	return c/2*((t-=2)*t*t*t*t + 2) + b;
 };
 
-
-
 function easeInOutSine (t, b, c, d) {
 	return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
 };
 
-
 function easeInQuad (t, b, c, d) {
     return c*(t/=d)*t + b;
 };
-
-
 
 // quadratic easing out - decelerating to zero velocity
 function easeOutQuad (t, b, c, d) {
@@ -69,85 +64,140 @@ function easeInCubic(t) {
     return Math.pow(t,3);
 }
 
-
 //THREEjs clock
 function getMilliseconds(clock){
     return Math.floor(clock.getElapsedTime()*1000);
 }
 
 
-function getImageData( image ) {
 
-    var canvas = document.createElement( 'canvas' );
-    canvas.width = image.width;
-    canvas.height = image.height;
 
-    var context = canvas.getContext( '2d' );
-    context.drawImage( image, 0, 0 );
 
-    return context.getImageData( 0, 0, image.width, image.height );
+
+// for setting another size
+function setSize(width,height,resolution){
+
+
+    if(debugLog)console.log("Setting new size: "+width+" "+height+" ratio "+resolution)
+
+    // Window and Pixel Vars
+     windowWidth=width;
+     windowHeight=height;
+     pixelScaleFact=resolution;
+     windowHalfY = windowHeight / 2;
+
+
+    // Create a Camera
+     left = -windowWidth/2; 
+     right = windowWidth/2;
+     topB = windowHeight/2;
+     bottom = -windowHeight/2;
+     near= 0;
+     far= 100;
+     camera = new THREE.OrthographicCamera( left, right, topB, bottom, near, far );
+
+    // remove old stuff
+    
+    scene.remove(vanegeometry);
+    scene.remove(lineGeometry);
+    scene.remove( thickline );
+    scene.remove( plane );
+
+    createVanes(diameter);
+
+    // set up geometry
+    vanegeometry = createGeometry();
+    vanegeometry.computeBoundingSphere();
+    lineGeometry = new THREE.LineSegmentsGeometry().setPositions( vanegeometry.attributes.position.array);
+    lineGeometry.setColors( vanegeometry.attributes.color.array);
+    // set up material
+    lineMaterial = new THREE.LineMaterial( { vertexColors: THREE.VertexColors, linewidth: 4.5} );
+    lineMaterial.resolution.set( windowWidth,windowHeight); // important, for now...
+     thickline = new THREE.LineSegments2( lineGeometry, lineMaterial );
+    scene.add( thickline );
+
+    plane = new THREE.Mesh( new THREE.PlaneGeometry( windowWidth, windowHeight ), new THREE.MeshBasicMaterial( { transparent: true, opacity: 0.25 } ) );
+    scene.add(plane);
+
+        
+    renderer.setPixelRatio( resolution );
+    renderer.setSize( width,height );
 
 }
 
-function getPixel( imagedata, x, y ) {
 
-    var position = ( x + imagedata.width * y ) * 4, data = imagedata.data;
-    return { r: data[ position ], g: data[ position + 1 ], b: data[ position + 2 ], a: data[ position + 3 ] };
 
-}
 
 
 function createVanes(vaneDiameter){
   vanes =[];
-  res = vaneDiameter;
-   var countX = Math.ceil(windowWidth/res);
-   var countY = Math.ceil(windowHeight/res);
-
+   var countX = Math.ceil(windowWidth/vaneDiameter);
+   var countY = Math.ceil(windowHeight/vaneDiameter);
     var xpos=-windowWidth/2
     var ypos=windowHeight/2
     var opX=0
     var opY=0
-
     for (var j = 0; j < countY; j++) {
         for (var i = 0; i < countX; i++) {
-            vanes.push( new WindVane((res*i)-windowWidth/2, (res*j)-windowHeight/2  ,vaneDiameter,clock,res*i,-res*j+windowHeight ));
+            vanes.push( new WindVane((vaneDiameter*i)-windowWidth/2, (vaneDiameter*j)-windowHeight/2  ,vaneDiameter,clock,vaneDiameter*i,-vaneDiameter*j+windowHeight ));
         }
     };
-
- 
-
   numberOfVanes=vanes.length;
-  console.log("num Vanes"+numberOfVanes);
+  if(debugLog)console.log("num Vanes"+numberOfVanes);
 }
+
+function removeEntity(object) {
+    var selectedObject = scene.getObjectById(object.name);
+    scene.remove( selectedObject );
+}
+
+
 
 
 function loadImages(imageurls,successCallback){
     var data=[];
-
     const loadManager = new THREE.LoadingManager();
-
     loadManager.onProgress = function ( item, loaded, total ) {
     // this gets called after any item has been loaded
-        console.log( 'Loading file: ' + item + '.\nLoaded ' + loaded + ' of ' + total + ' files.' );
+        if(debugLog)console.log( 'Loading file: ' + item + '.\nLoaded ' + loaded + ' of ' + total + ' files.' );
     };
 
     loadManager.onLoad = function () {
         console.log( 'Loading complete!');
         console.log(data.length);
         successCallback(data)
-
-
     };
 
-
     const multiloader = new THREE.ImageLoader(loadManager);
-
     for(var i=0;i<imageurls.length;i++){
         multiloader.load( imageurls[i], function ( image ) {
             data.push( image );
         });
     }
 }
+
+function getImageData( image ) {
+    var canvas = document.createElement( 'canvas' );
+    canvas.width = image.width;
+    canvas.height = image.height;
+    var context = canvas.getContext( '2d' );
+    context.drawImage( image, 0, 0 );
+    return context.getImageData( 0, 0, image.width, image.height );
+}
+
+function getPixel( imagedata, x, y ) {
+    var position = ( x + imagedata.width * y ) * 4, data = imagedata.data;
+    return { r: data[ position ], g: data[ position + 1 ], b: data[ position + 2 ], a: data[ position + 3 ] };
+
+}
+
+function cycleImages(){
+    imageIndex=(imageIndex+1)%imagesData.length
+    //setMask(imagesData[imageIndex]);
+    if(debugLog)console.log("cycle!"+imageIndex);
+}
+
+
 
 
 
